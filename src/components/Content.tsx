be6b4cce67207bdfd5efc4efaacd37e378a8b6fb
chubbyskink <gavinolsenai@gmail.com>
2024-03-11 16:57:16 -0700

    cleanup

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,17 +9,21 @@ export const Content = () => {
             }}
         >
             <ScrollingText>
-                <Typography>
+                <Typography
+                    sx={{ position: "absolute" }}
+                >
                     Next-gen montessori education
                 </Typography>
             </ScrollingText>
             <ScrollingText>
-                <Typography>
+                <Typography
+                    sx={{ position: "absolute" }}
+                >
                     Montessori: education philosophy and practice that fosters rigorous, self-motivated growth for children and adolescents in all areas of their development, with a goal of nurturing each child's natural desire for knowledge, understanding, and respect.
                 </Typography>
             </ScrollingText>
             <Link
-                variant="h1"
+                variant="h4"
                 sx={{
                     cursor: "pointer",
                     paddingBottom: 10,
@@ -30,22 +34,4 @@ export const Content = () => {
             </Link>
         </Box>
     );
-}
-
-interface BigTextProps {
-    text: string;
-    sx?: SxProps<Theme>;
-}
-const BigText = ({ text, sx }: BigTextProps) => {
-    return (
-        <Typography
-            variant="h2"
-            sx={{
-                paddingBottom: 50,
-                ...sx,
-            }}
-        >
-            {text}
-        </Typography>
-    )
 }
