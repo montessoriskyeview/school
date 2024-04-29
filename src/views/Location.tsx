cb013174a16dbc6117413b4b108261ddeea4cacd
chubbyskink <famgavolsen@gmail.com>
2024-04-29 13:48:10 -0700

    alt fix

--- src/views/Location.tsx
+++ src/views/Location.tsx
@@ -20,7 +20,6 @@ function srcset(image: string, size: number, rows = 1, cols = 1) {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
     srcSet: `${image}?w=${size * cols}&h=${size * rows
       }&fit=crop&auto=format&dpr=2 2x`,
-    alt: "school photos"
   };
 }
 
@@ -53,11 +52,12 @@ export const Location = () => {
                 cols={4}
                 rowHeight={121}
               >
-                {itemData.map((item) => (
+                {itemData.map((item, i) => (
                   <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
                     <img
                       {...srcset(item.img, 121, item.rows, item.cols)}
                       loading="lazy"
+                      alt={"school photos-"+i}
                     />
                   </ImageListItem>
                 ))}
