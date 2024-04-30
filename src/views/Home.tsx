a220687bef24884e0019af83f57fde365b667814
chubbyskink <famgavolsen@gmail.com>
2024-04-29 19:16:21 -0700

    age change

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,7 +11,7 @@ export const Home = () => {
       />
       <InfoText
         title="About us"
-        text="Montessori Skye View is a new Montessori school dedicated to fostering a love of learning in children aged 3 and up. We believe in the power of the Montessori Method, an educational approach developed by Dr. Maria Montessori, which emphasizes self-directed exploration, hands-on activities, and a nurturing environment. Located in the northwest corner of Las Vegas, our beautiful Skye Canyon campus provides ample outdoor space for play and learning."
+        text="Montessori Skye View is a new Montessori school dedicated to fostering a love of learning for grades pre-k through 8th. We believe in the power of the Montessori Method, an educational approach developed by Dr. Maria Montessori, which emphasizes self-directed exploration, hands-on activities, and a nurturing environment. Located in the northwest corner of Las Vegas, our beautiful Skye Canyon campus provides ample outdoor space for play and learning."
       />
     </CanvasView>
   )
