f7dd02c45770de9d430b44ab88de49189faf9c70
chubbywallababy <famgavolsen@gmail.com>
2024-08-02 08:01:05 -0700

    date change

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -11,70 +11,70 @@ export const Registration = () => {
   const theme = useTheme();
   return (
     <CanvasView>
-      <Box component={"div"}>
+      <Box component={'div'}>
         <InfoText
-          text={"Reservations and payments are currently accepted through the individual forms found in the sections below"}
+          text={
+            'Reservations and payments are currently accepted through the individual forms found in the sections below'
+          }
         />
         <CollapseContainer
-          title="Current registration - Summer camp"
+          title='Current registration - Summer camp'
           content={
             <>
               <SchoolList
-                items={
-                  [
-                    "Unleash your child's inner explorer at our action-packed summer camp! Make new friends, discover new talents, and create lasting memories through exciting activities and outdoor adventures. Arts and crafts supplies, sports games, musical workshops, snacks, and water activities all included with weekly fees. Classes are held from M-TR, 9:00 am - 1:00 pm. Please pack a lunch and water bottle for your child.",
-                    "Program costs are $125/week and enrollment is on a first-come, first-serve basis. Spots will be held as registration and form of payment is provided. 10% discounts applied for siblings.",
-                    <Link
-                      variant="body1"
-                      sx={{
-                        cursor: "pointer",
-                        color: "#09559f"
-                      }}
-                      href={SUMMER_CAMP_LINK}
-                    >
-                      Register here
-                    </Link>
-                  ]
-                }
+                items={[
+                  "Unleash your child's inner explorer at our action-packed summer camp! Make new friends, discover new talents, and create lasting memories through exciting activities and outdoor adventures. Arts and crafts supplies, sports games, musical workshops, snacks, and water activities all included with weekly fees. Classes are held from M-TR, 9:00 am - 1:00 pm. Please pack a lunch and water bottle for your child.",
+                  'Program costs are $125/week and enrollment is on a first-come, first-serve basis. Spots will be held as registration and form of payment is provided. 10% discounts applied for siblings.',
+                  <Link
+                    variant='body1'
+                    sx={{
+                      cursor: 'pointer',
+                      color: '#09559f'
+                    }}
+                    href={SUMMER_CAMP_LINK}
+                  >
+                    Register here
+                  </Link>
+                ]}
               />
               <ImageContainer
                 style={{
                   padding: 10,
-                  display: "flex",
-                  alignContent: "center",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  display: 'flex',
+                  alignContent: 'center',
+                  justifyContent: 'center',
+                  alignItems: 'center'
                 }}
                 src={flyer}
-                alt="Registration image"
+                alt='Registration image'
                 imgSx={{
                   [theme.breakpoints.down('md')]: {
-                    height: "400px",
-                    width: "350px",
+                    height: '400px',
+                    width: '350px'
                   },
                   [theme.breakpoints.up('md')]: {
-                    height: "700px",
-                    width: "600px",
-                  },
+                    height: '700px',
+                    width: '600px'
+                  }
                 }}
               />
             </>
           }
         />
         <CollapseContainer
-          title="Fall 2024"
+          title='Fall 2024'
           content={
             <SchoolList
               items={[
-                "Starting September 3rd",
-                "$400 Deposit Fee (includes registration) before July 1st",
-                "$500 Deposit Fee (includes registration) after July 1st",
-                "Current limit: 45 total students",
+                'Starting September 3rd',
+                '$400 Deposit Fee (includes registration) before August 31st',
+                '$500 Deposit Fee (includes registration) after August 31st',
+                'Current limit: 45 total students',
                 <Link
-                  variant="body1"
+                  variant='body1'
                   sx={{
-                    cursor: "pointer",
-                    color: "#09559f",
+                    cursor: 'pointer',
+                    color: '#09559f'
                   }}
                   href={FALL_2024_LINK}
                 >
@@ -86,5 +86,5 @@ export const Registration = () => {
         />
       </Box>
     </CanvasView>
-  )
+  );
 }
--- src/views/Tuition.tsx
+++ src/views/Tuition.tsx
@@ -7,56 +7,60 @@ export const Tuition = () => {
   return (
     <CanvasView>
       <InfoText
-        text={"Payment is currently accepted through the forms in the registration page (from the menu)"}
+        text={
+          'Payment is currently accepted through the forms in the registration page (from the menu)'
+        }
       />
       <CollapseContainer
-        title="Part-time Core Day Only"
-        content={<SchoolList
-          items={[
-            "$6,750 Paid in Full",
-            "Or $612.50 monthly paid over 12 months",
-            "Monday through Thursday - 9am to 1pm",
-          ]}
-        />}
-      />
-      <CollapseContainer
-        title="Full-time School Day"
-        content={<SchoolList
-          items={[
-            "$9,900 paid in full",
-            "Or $875 monthly paid over 12 months",
-            "Monday through Friday - 8am to 4pm"
-          ]}
-        />}
-      />
-      <CollapseContainer
-        title="Deposits"
-        content={<SchoolList
-          items={[
-            "$400 Deposit Fee (includes registration) before July 1st",
-            "$500 Deposit Fee (includes registration) after July 1st",
-          ]}
-        />}
-
-      />
-      <CollapseContainer
-        title="Tuition includes"
+        title='Part-time Core Day Only'
         content={
           <SchoolList
             items={[
-              "Curriculum",
-              "Fall/Spring school pictures",
-              "Monthly “Fun for Everyone” activities",
-              "Snacks",
-              "Supplies",
+              '$6,750 Paid in Full',
+              'Or $612.50 monthly paid over 12 months',
+              'Monday through Thursday - 9am to 1pm'
             ]}
           />
         }
       />
-      <InfoText
-        title="School Calendar Year"
-        text="September 3rd - May 30"
+      <CollapseContainer
+        title='Full-time School Day'
+        content={
+          <SchoolList
+            items={[
+              '$9,900 paid in full',
+              'Or $875 monthly paid over 12 months',
+              'Monday through Friday - 8am to 4pm'
+            ]}
+          />
+        }
       />
+      <CollapseContainer
+        title='Deposits'
+        content={
+          <SchoolList
+            items={[
+              '$400 Deposit Fee (includes registration) before August 31st',
+              '$500 Deposit Fee (includes registration) after August 31st'
+            ]}
+          />
+        }
+      />
+      <CollapseContainer
+        title='Tuition includes'
+        content={
+          <SchoolList
+            items={[
+              'Curriculum',
+              'Fall/Spring school pictures',
+              'Monthly “Fun for Everyone” activities',
+              'Snacks',
+              'Supplies'
+            ]}
+          />
+        }
+      />
+      <InfoText title='School Calendar Year' text='September 3rd - May 30' />
     </CanvasView>
-  )
+  );
 }
